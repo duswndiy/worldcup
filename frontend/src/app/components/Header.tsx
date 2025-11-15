eff014cb6b8955715093865b54825c837fac5701
duswndiy <wnmkwak@gmail.com>
2025-11-15 21:30:55 +0900

    feat: 로고 클릭 시, 루트페이지로 이동하도록 next/link 연결

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,11 +1,14 @@
 import { DarkToggle } from "./dark-toggle";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <header className="border-b border-slate-800">
         <div className="mx-auto max-w-5xl px-6 py-4">
-            <h1 className="text-lg font-semibold">Worldcup</h1>
-            <DarkToggle />
+            <Link href="/">
+                <h1 className="text-lg font-semibold">Worldcup</h1>
+                <DarkToggle />
+            </Link>
         </div>
         </header>
     );
