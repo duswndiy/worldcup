6d0494716582a495262f25c8061ca32b0648c241
duswndiy <wnmkwak@gmail.com>
2025-11-20 20:24:47 +0900

    style: 공통 레이아웃 UI 개선

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <body className="min-h-screen bg-background text-foreground antialiased">
         <ThemeProvider>
             <Header />
-            <main className="mx-auto max-w-[1500px] px-8 py-6">{children}</main>
+            <main className="mx-auto max-w-[1500px] min-h-[1300px] rounded-xl border mt-2 mb-6 p-14">{children}</main>
             <Footer />
         </ThemeProvider>
         </body>
